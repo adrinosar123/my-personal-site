1ff4b516132f9d7a9230c0e881ecdb95c58888c3
kusal dotel <kusaldotel@192-168-1-128.tpgi.com.au>
2020-10-15 23:31:28 +1100

    newsletter test

---
+++ src/components/Newsletter.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+class Newsletter extends React.Component{
+
+    render(){
+        return (
+            <>
+            <div className="News">
+                <p>you will receive</p>
+                <div className ="form">
+                    <form className="subscribe" onSubmit={}>
+                        <input type="email" value={} 
+                        onChange={} name="EMAIL" i
+                        d="n-EMAIL" className="subscribe-email" 
+                        placeholder="o to EMAIL" required />
+                        <button className="btn_send" type="submit">Get your ebook</button>
+                    </form>
+                </div>
+            </div>
+            </>
+        )
+    }
+}
+export default Newsletter
